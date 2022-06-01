e12af0a57175b94a8ef2e33968ff9a1419a4e4a2
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2022-06-02 00:03:45 +0300

    favicon added

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './assets/fonts/AvenirRegular.otf'
+import './assets/fonts/TiemposBold.otf'
+import './assets/fonts/TiemposRegular.otf'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
